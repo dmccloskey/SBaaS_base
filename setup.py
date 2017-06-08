a8fc9122fca65e6bc27441986058fc63b9be1fcc
dmccloskey <dmccloskey87@gmail.com>
2017-06-08 07:13:58 -0700

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="SBaaS_base",
+    version="1.0.b1",
+    description="Library of postgresql and sqlalchemy methods",
+    author="Douglas McCloskey",
+    author_email="dmccloskey87@gmail.com",
+    url="https://github.com/dmccloskey/SBaaS_base",
+    packages=["SBaaS_base"],
+    #classifiers=[
+    #'Development Status :: 5 - Production/Stable',
+    #'Environment :: Console',
+    #'Intended Audience :: Science/Research',
+    #'Operating System :: OS Independent',
+    #'Programming Language :: Python :: 3.4',
+    #'Programming Language :: Cython',
+    #'Programming Language :: Python :: Implementation :: CPython',
+    #'Topic :: Scientific/Engineering',
+    #'Topic :: Scientific/Engineering :: Bio-Informatics'
+    #],
+    platforms="GNU/Linux, Mac OS X >= 10.7, Microsoft Windows >= 7",
+    )
